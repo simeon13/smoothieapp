e739367f15f11b1b01be69b48cc4c4b82e117692
simeon13 <33508230+simeon13@users.noreply.github.com>
2020-02-03 20:53:36 -0800

    database

--- smoothieapp/AppDelegate.swift
+++ smoothieapp/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var ref: DatabaseReference!
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
