33b92a1bda1635c02e4c6ac82bded5bb4c9b9904
simeon13 <33508230+simeon13@users.noreply.github.com>
2020-02-09 16:09:35 -0800

    added changes

--- smoothieapp/FoodView.swift
+++ smoothieapp/FoodView.swift
@@ -9,18 +9,23 @@
 import SwiftUI
 import FirebaseDatabase
 
-//let recipeRef = Database.database().reference(withPath: "recipes")
-
 struct FoodView: View {
+    @ObservedObject var recipes = observer()
+    
     var body: some View {
-        
         NavigationView {
             List {
-                Text("Hello World")
-                Text("Hello World")
-                Text("Hello World")
+                // Recipes
+                Section(header: Text("Recipes")) {
+                    ForEach(recipes.data){i in
+                        Text(i.name)
+                    }
+                }
+                
+                // Foods
             }
-            .navigationBarTitle("Menu")
+            .navigationBarTitle("Food")
+            .listStyle(GroupedListStyle())
         }
     }
 }
@@ -29,4 +34,37 @@ struct FoodView_Previews: PreviewProvider {
     static var previews: some View {
         FoodView()
     }
+}
+
+class observer : ObservableObject {
+    @Published var data = [Recipe]()
+    
+    init(){
+        let ref = Database.database().reference(withPath: "recipes")
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children.allObjects as! [DataSnapshot] {
+                let value = child.value as? NSDictionary
+                let name = value?["name"] as? String ?? ""
+                let url = value?["url"] as? String ?? ""
+                let info = Recipe(id: name, name: name, url: url)
+                print(name)
+                self.data.append(info)
+            }
+        })
+//            let value = snapshot.value as? NSDictionary
+//            let name = value?["name"] as? String ?? ""
+//            let url = value?["url"] as? String ?? ""
+//            let info = Recipe(id: "hi", name: name, url: url)
+//            self.data.append(info)
+//           ...
+//          }) { (error) in
+//            print(error.localizedDescription)
+//        }
+    }
+}
+
+struct Recipe : Identifiable {
+    var id : String
+    var name : String
+    var url : String
 }
