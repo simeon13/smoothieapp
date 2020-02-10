11077a2c082bfab15463966e0e7827a091b97168
simeon13 <33508230+simeon13@users.noreply.github.com>
2020-02-10 13:13:52 -0800

    added every food and recipe item to the screen

--- smoothieapp/FoodView.swift
+++ smoothieapp/FoodView.swift
@@ -10,14 +10,14 @@ import SwiftUI
 import FirebaseDatabase
 
 struct FoodView: View {
-    @ObservedObject var recipes = observer()
+    @ObservedObject var data = observer()
     
     var body: some View {
         NavigationView {
             List {
                 // Recipes
                 Section(header: Text("Recipes")) {
-                    ForEach(recipes.data){i in
+                    ForEach(data.recipe_data){i in
                         NavigationLink(destination: RecipeView(recipe: i)) {
                             RecipeRow(recipe: i)
                         }
@@ -25,8 +25,55 @@ struct FoodView: View {
                 }
                 
                 // Foods
+                Section(header: Text("Vegetables")) {
+                    ForEach(data.vegetable_data){i in
+                        NavigationLink(destination: FoodItemView(foodItem: i)) {
+                            FoodItemRow(foodItem: i)
+                        }
+                    }
+                }
+                
+                Section(header: Text("Dairy")) {
+                    ForEach(data.dairy_data){i in
+                        NavigationLink(destination: FoodItemView(foodItem: i)) {
+                            FoodItemRow(foodItem: i)
+                        }
+                    }
+                }
+                
+                Section(header: Text("Fish")) {
+                    ForEach(data.fish_data){i in
+                        NavigationLink(destination: FoodItemView(foodItem: i)) {
+                            FoodItemRow(foodItem: i)
+                        }
+                    }
+                }
+                
+                Section(header: Text("Fruits")) {
+                    ForEach(data.fruit_data){i in
+                        NavigationLink(destination: FoodItemView(foodItem: i)) {
+                            FoodItemRow(foodItem: i)
+                        }
+                    }
+                }
+                
+                Section(header: Text("Grains")) {
+                    ForEach(data.grain_data){i in
+                        NavigationLink(destination: FoodItemView(foodItem: i)) {
+                            FoodItemRow(foodItem: i)
+                        }
+                    }
+                }
+                
+                Section(header: Text("Poultry")) {
+                    ForEach(data.poultry_data){i in
+                        NavigationLink(destination: FoodItemView(foodItem: i)) {
+                            FoodItemRow(foodItem: i)
+                        }
+                    }
+                }
             }
-            .navigationBarTitle("Food")
+            .navigationBarTitle("Items")
             .listStyle(GroupedListStyle())
         }
     }
@@ -38,24 +85,18 @@ struct FoodView_Previews: PreviewProvider {
     }
 }
 
-struct RecipeView: View {
-    var recipe: Recipe
-    var body: some View {
-        Text("Working")
-    }
-}
-
-struct RecipeRow: View {
-    var recipe: Recipe
-    var body: some View {
-        Text(recipe.name)
-    }
-}
-
 class observer : ObservableObject {
-    @Published var data = [Recipe]()
+    @Published var recipe_data = [Recipe]()
+    @Published var vegetable_data = [FoodItem]()
+    @Published var dairy_data = [FoodItem]()
+    @Published var fish_data = [FoodItem]()
+    @Published var fruit_data = [FoodItem]()
+    @Published var grain_data = [FoodItem]()
+    @Published var poultry_data = [FoodItem]()
+    
     
     init(){
+        // RECIPES
         let ref = Database.database().reference(withPath: "recipes")
         ref.observeSingleEvent(of: .value, with: { (snapshot) in
             for child in snapshot.children.allObjects as! [DataSnapshot] {
@@ -78,9 +119,69 @@ class observer : ObservableObject {
                 let zinc = value?["zinc"] as? String ?? ""
                 // create new recipe object
                 let info = Recipe(id: name, name: name, url: url, image: image, calcium: calcium, fiber: fiber, iron: iron, magnesium: magnesium, potassium: potassium, protein: protein, vitaminA: vitaminA, vitaminB12: vitaminB12, vitaminC: vitaminC, vitaminD: vitaminD, vitaminE: vitaminE, vitaminK: vitaminK, zinc: zinc)
-                self.data.append(info)
+                self.recipe_data.append(info)
             }
         })
+        
+        // FOODS
+        let food_list = ["Vegetables", "dairy", "fish", "fruit", "grains", "poultry"]
+        for entry in food_list {
+            let food_ref = Database.database().reference(withPath: entry)
+            food_ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                for child in snapshot.children.allObjects as! [DataSnapshot] {
+                    let value = child.value as? NSDictionary
+                    let name = child.key.capitalized
+                    let calcium = value?["calcium"] as? String ?? ""
+                    let fiber = value?["fiber"] as? String ?? ""
+                    let iron = value?["iron"] as? String ?? ""
+                    let magnesium = value?["magnesium"] as? String ?? ""
+                    let potassium = value?["potassium"] as? String ?? ""
+                    let protein = value?["protein"] as? String ?? ""
+                    let vitaminA = value?["vitaminA"] as? String ?? ""
+                    let vitaminB12 = value?["vitaminB12"] as? String ?? ""
+                    let vitaminC = value?["vitaminC"] as? String ?? ""
+                    let vitaminD = value?["vitaminD"] as? String ?? ""
+                    let vitaminE = value?["vitaminE"] as? String ?? ""
+                    let vitaminK = value?["vitaminK"] as? String ?? ""
+                    let zinc = value?["zinc"] as? String ?? ""
+                    // create new foodItem object
+                    let food_item = FoodItem(id: name, name: name, calcium: calcium, fiber: fiber, iron: iron, magnesium: magnesium, potassium: potassium, protein: protein, vitaminA: vitaminA, vitaminB12: vitaminB12, vitaminC: vitaminC, vitaminD: vitaminD, vitaminE: vitaminE, vitaminK: vitaminK, zinc: zinc)
+                    if (entry == "Vegetables"){
+                        self.vegetable_data.append(food_item)
+                    }
+                    if (entry == "dairy"){
+                        self.dairy_data.append(food_item)
+                    }
+                    if (entry == "fish"){
+                        self.fish_data.append(food_item)
+                    }
+                    if (entry == "fruit"){
+                        self.fruit_data.append(food_item)
+                    }
+                    if (entry == "grains"){
+                        self.grain_data.append(food_item)
+                    }
+                    if (entry == "poultry"){
+                        self.poultry_data.append(food_item)
+                    }
+                }
+            })
+        }
+        
+    }
+}
+
+struct RecipeView: View {
+    var recipe: Recipe
+    var body: some View {
+        Text("Recipe Working")
+    }
+}
+
+struct RecipeRow: View {
+    var recipe: Recipe
+    var body: some View {
+        Text(recipe.name)
     }
 }
 
@@ -89,6 +190,38 @@ struct Recipe : Identifiable {
     var name : String
     var url : String
     var image : String
+    var calcium : String
+    var fiber : String
+    var iron : String
+    var magnesium : String
+    var potassium : String
+    var protein : String
+    var vitaminA : String
+    var vitaminB12 : String
+    var vitaminC : String
+    var vitaminD : String
+    var vitaminE : String
+    var vitaminK : String
+    var zinc : String
+}
+
+struct FoodItemView: View {
+    var foodItem : FoodItem
+    var body: some View {
+        Text("Food Item Working")
+    }
+}
+
+struct FoodItemRow: View {
+    var foodItem : FoodItem
+    var body: some View {
+        Text(foodItem.name)
+    }
+}
+
+struct FoodItem : Identifiable {
+    var id : String
+    var name : String
     var calcium : String
     var fiber : String
     var iron : String
