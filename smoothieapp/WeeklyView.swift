15d3d58098bbd6fc1cee98ab0817e8338443ff96
simeon13 <33508230+simeon13@users.noreply.github.com>
2020-02-14 13:46:06 -0800

    added new file for weekly view

---
+++ smoothieapp/WeeklyView.swift
@@ -0,0 +1,21 @@
+//
+//  WeeklyView.swift
+//  smoothieapp
+//
+//  Created by Simeon Lam on 2/14/20.
+//  Copyright © 2020 cs125. All rights reserved.
+//
+
+import SwiftUI
+
+struct WeeklyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeeklyView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeeklyView()
+    }
+}
