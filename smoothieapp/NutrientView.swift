8478c5320f6ae25b392f6ef378871324d67298f4
simeon13 <33508230+simeon13@users.noreply.github.com>
2020-02-15 14:54:54 -0800

    added units class and changes to nutrient view

--- smoothieapp/NutrientView.swift
+++ smoothieapp/NutrientView.swift
@@ -27,34 +27,37 @@ struct NutrientView: View {
     var body: some View {
         ScrollView{
             VStack{
+                Spacer()
+                .frame(height: 50.0)
+                
                 Group {
-                    progressBar(total: $userInfo.total_values.calcium, max: $userInfo.max_values.calcium, name: $calcium)
+                    progressBar(total: $userInfo.total_values.calcium, max: $userInfo.max_values.calcium, name: $calcium, unit: $userInfo.nutrient_units.calcium)
                     
-                    progressBar(total: $userInfo.total_values.fiber, max: $userInfo.max_values.fiber, name: $fiber)
+                    progressBar(total: $userInfo.total_values.fiber, max: $userInfo.max_values.fiber, name: $fiber, unit: $userInfo.nutrient_units.fiber)
 
-                    progressBar(total: $userInfo.total_values.iron, max: $userInfo.max_values.iron, name: $iron)
+                    progressBar(total: $userInfo.total_values.iron, max: $userInfo.max_values.iron, name: $iron, unit: $userInfo.nutrient_units.iron)
 
-                    progressBar(total: $userInfo.total_values.magnesium, max: $userInfo.max_values.magnesium, name: $magnesium)
+                    progressBar(total: $userInfo.total_values.magnesium, max: $userInfo.max_values.magnesium, name: $magnesium, unit: $userInfo.nutrient_units.magnesium)
 
-                    progressBar(total: $userInfo.total_values.potassium, max: $userInfo.max_values.potassium, name: $potassium)
+                    progressBar(total: $userInfo.total_values.potassium, max: $userInfo.max_values.potassium, name: $potassium, unit: $userInfo.nutrient_units.potassium)
 
-                    progressBar(total: $userInfo.total_values.protein, max: $userInfo.max_values.protein, name: $protein)
+                    progressBar(total: $userInfo.total_values.protein, max: $userInfo.max_values.protein, name: $protein, unit: $userInfo.nutrient_units.protein)
                     
-                    progressBar(total: $userInfo.total_values.zinc, max: $userInfo.max_values.zinc, name: $zinc)
+                    progressBar(total: $userInfo.total_values.zinc, max: $userInfo.max_values.zinc, name: $zinc, unit: $userInfo.nutrient_units.zinc)
                 }
 
                 Group {
-                    progressBar(total: $userInfo.total_values.vitaminA, max: $userInfo.max_values.vitaminA, name: $vitaminA)
+                    progressBar(total: $userInfo.total_values.vitaminA, max: $userInfo.max_values.vitaminA, name: $vitaminA, unit: $userInfo.nutrient_units.vitaminA)
 
-                    progressBar(total: $userInfo.total_values.vitaminB12, max: $userInfo.max_values.vitaminB12, name: $vitaminB12)
+                    progressBar(total: $userInfo.total_values.vitaminB12, max: $userInfo.max_values.vitaminB12, name: $vitaminB12, unit: $userInfo.nutrient_units.vitaminB12)
 
-                    progressBar(total: $userInfo.total_values.vitaminC, max: $userInfo.max_values.vitaminC, name: $vitaminC)
+                    progressBar(total: $userInfo.total_values.vitaminC, max: $userInfo.max_values.vitaminC, name: $vitaminC, unit: $userInfo.nutrient_units.vitaminC)
 
-                    progressBar(total: $userInfo.total_values.vitaminD, max: $userInfo.max_values.vitaminD, name: $vitaminD)
+                    progressBar(total: $userInfo.total_values.vitaminD, max: $userInfo.max_values.vitaminD, name: $vitaminD, unit: $userInfo.nutrient_units.vitaminD)
 
-                    progressBar(total: $userInfo.total_values.vitaminE, max: $userInfo.max_values.vitaminE, name: $vitaminE)
+                    progressBar(total: $userInfo.total_values.vitaminE, max: $userInfo.max_values.vitaminE, name: $vitaminE, unit: $userInfo.nutrient_units.vitaminE)
         
-                    progressBar(total: $userInfo.total_values.vitaminK, max: $userInfo.max_values.vitaminK, name: $vitaminK)
+                    progressBar(total: $userInfo.total_values.vitaminK, max: $userInfo.max_values.vitaminK, name: $vitaminK, unit: $userInfo.nutrient_units.vitaminK)
                 }
             }
             .navigationBarTitle("Nutrient Levels", displayMode: .inline)
@@ -72,6 +75,7 @@ struct progressBar : View {
     @Binding var total : CGFloat
     @Binding var max : CGFloat
     @Binding var name : String
+    @Binding var unit : String
     
     var body : some View {
         VStack(spacing: 10) {
@@ -92,7 +96,7 @@ struct progressBar : View {
                     .padding(.bottom, 15.0)
                     .frame(maxWidth: .infinity, alignment: .leading)
                 
-                Text(format(total: total, max: max))
+                Text(format(total: total, max: max, unit: unit))
                     .padding(.trailing, 35.0)
                     .padding(.bottom, 15.0)
                     .frame(maxWidth: .infinity, alignment: .trailing)
@@ -102,7 +106,7 @@ struct progressBar : View {
     }
 }
 
-func format(total: CGFloat, max: CGFloat) -> String{
-    let formatted = String(format: "%.2f / %.f", total, max)
+func format(total: CGFloat, max: CGFloat, unit: String) -> String{
+    let formatted = String(format: "%.2f / %.f \(unit)", total, max)
     return formatted
 }
